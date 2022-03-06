1f367a5123e2a3a07a5e5a0a59551fddbd50baab
sboucher <sboucher2011@gmail.com>
2022-03-06 09:53:38 -0500

    added footer links

--- react-portfolio-hire-me/src/components/PortfolioContainer.js
+++ react-portfolio-hire-me/src/components/PortfolioContainer.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+
 import NavTabs from './NavTabs';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -29,6 +31,17 @@ export default function PortfolioContainer() {
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
+      <footer>
+      <a href="https://github.com/sboucher2011">
+        <img src="src/assets/github.png" className="footerIcon" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/shawna-boucher-a10b9662/">
+        <img src="src/assets/linkedIn.png" className="footerIcon" />
+      </a>
+
+      </footer>
     </div>
+    
   );
 }
