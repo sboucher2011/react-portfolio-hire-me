c7b66f47bd0d91684f6c9dabb4c1743f8b711617
sboucher <sboucher2011@gmail.com>
2022-03-06 08:48:13 -0500

    added content to resume section

--- react-portfolio-hire-me/src/components/pages/Resume.js
+++ react-portfolio-hire-me/src/components/pages/Resume.js
@@ -4,7 +4,43 @@ function Resume() {
   return (
     <div>
       <h1>Resume</h1>
+
       
+      <a href="/src/assets/download/resume.doc" download>My Resume</a>
+
+    <ul>
+        <p>Web Development Certificate from Univeristy of Central Florida</p>
+        <li>Front End Skills</li>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Handlebars</li>
+            <li>REACT</li>
+        </ul>
+
+        <li>Backend End Skills</li>
+        <ul>
+            <li>Node</li>
+            <li>SQL</li>
+            <li>NoSQL - Mongoose & Firebase</li>
+            <li>Express</li>
+            <li>APIs</li>
+            <li>Sequalize</li>
+        </ul>
+    </ul>
+
+    <ul>
+    <li>Other Languges</li>
+        <ul>
+            <li>Swift</li>
+            <li>C#</li>
+            <li>Blazor</li>
+            <li>VBA</li>
+            <li>Arduino</li>
+        </ul>
+    </ul>
+
     </div>
   );
 }
