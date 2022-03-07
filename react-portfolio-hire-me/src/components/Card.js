21663873cd459b979f2375b6431f3b15d5036f0b
sboucher <sboucher2011@gmail.com>
2022-03-07 17:26:48 -0500

    added pictures for apps and cleaned up text

--- react-portfolio-hire-me/src/components/Card.js
+++ react-portfolio-hire-me/src/components/Card.js
@@ -2,32 +2,27 @@ import React from 'react';
 
 export default function Card(props) {
   const cardStyle = {
-    width: '18rem',
-  };
-
-  // Helper function that generates a random width for our placeholder images
-  const randomWidth = () => {
-    const number = Math.random() * (300 - 200) + 200;
-    return number.toString().split('.')[0];
+    width: '50rem',
   };
 
   return (
     <div className="container">
       <div className="card" style={cardStyle}>
-      <a href={props.github} className="btn btn-primary">
-            {props.name}
-          </a>
-        <a href={props.workingPage}>
-            <img
-            className="card-img-top"
-            src={`http://placecorgi.com/${randomWidth()}`}
-            alt="Card"
-            />
-        </a>    
-        <div className="card-body">
-            <p className="card-text">Skills: {props.description}</p>
+        <a href={props.github} className="btn btn-primary">
+                {props.name}
+            </a>
+            <a href={props.workingPage}>
+                <img
+                className="card-img-top"
+                //src={`http://placecorgi.com/${randomWidth()}`}
+                src = {require(`../assets/images/${props.photo}.jpg`)}
+                alt="Card"
+                />
+            </a>    
+            <div className="card-body">
+                <p className="card-text">Skills: {props.description}</p>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
--- react-portfolio-hire-me/src/components/pages/Portfolio.js
+++ react-portfolio-hire-me/src/components/pages/Portfolio.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Card from '../Card';
 
 let projects = [
-  {id: "1", name: "Run Buddy", description: "HTML & CSS", image: "link", github: "https://github.com/sboucher2011/run-buddy", workingPage: "https://lernantino.github.io/run-buddy/"},
-  {id: "2", name: "Weather Dashboard", description: "APIs", image: "link", github: "https://github.com/sboucher2011/weather-dashboard", workingPage: "https://sboucher2011.github.io/weather-dashboard/"},
-  {id: "3", name: "Event Finder", description: "APIs", image: "link", github: "https://github.com/ivydo/Event-Finder", workingPage: "https://ivydo.github.io/Event-Finder/"},
-  {id: "4", name: "Pizza Hunt", description: "NoSQL", image: "link", github: "https://github.com/sboucher2011/pizza-hunt", workingPage: "https://pizza-hunt-app-88.herokuapp.com/"},
-  {id: "5", name: "Budget Tracker", description: "Offline & PWA", image: "link", github: "https://github.com/sboucher2011/pwa-Budget-tracker", workingPage: "https://floating-fortress-65122.herokuapp.com/"},
-  {id: "6", name: "Retro Rewind", description: "Sequalize & Express", image: "link", github: "https://github.com/kelsey-schenk/retro-rewind", workingPage: "https://ivydo.github.io/Event-Finder/"}
+  {id: "1", name: "Task Master", description: "HTML, CSS, Javascript", photo: "taskMaster", github: "https://github.com/sboucher2011/taskmaster-pro", workingPage: "https://sboucher2011.github.io/taskmaster-pro/"},
+  {id: "2", name: "Weather Dashboard", description: "APIs", photo: "weatherDashboard", github: "https://github.com/sboucher2011/weather-dashboard", workingPage: "https://sboucher2011.github.io/weather-dashboard/"},
+  {id: "3", name: "Event Finder", description: "APIs", photo: "eventFinder", github: "https://github.com/ivydo/Event-Finder", workingPage: "https://ivydo.github.io/Event-Finder/"},
+  {id: "4", name: "Pizza Hunt", description: "NoSQL", photo: "pizzaHunt", github: "https://github.com/sboucher2011/pizza-hunt", workingPage: "https://pizza-hunt-app-88.herokuapp.com/"},
+  {id: "5", name: "Budget Tracker", description: "Offline & PWA", photo: "budgetTracker", github: "https://github.com/sboucher2011/pwa-Budget-tracker", workingPage: "https://floating-fortress-65122.herokuapp.com/"},
+  {id: "6", name: "Retro Rewind", description: "Sequalize & Express", photo: "retrorewind", github: "https://github.com/kelsey-schenk/retro-rewind", workingPage: "https://retro-rewind.herokuapp.com/"}
 ];
 
 function Portfolio() {
@@ -18,7 +18,7 @@ function Portfolio() {
 
       <section>
         {projects.map( (project, index) => (
-          <Card name={project.name} description={project.description} id={project.id} github={project.github} workingPage={project.workingPage}/>
+          <Card name={project.name} description={project.description} photo={project.photo} id={project.id} github={project.github} workingPage={project.workingPage}/>
         ))}
       </section>
 
